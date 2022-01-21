5d8945b397fc3bc5544cfd888c384059ec771390
SampoLS <mr.sampo@outlook.com>
2022-01-21 15:12:09 +0800

    updated

--- src/components/SplitPage.js
+++ src/components/SplitPage.js
@@ -4,8 +4,11 @@ import getSinglePageContents from "../utils/getSinglePageContents";
 const SplitPage = ({ content }) => {
   const { pId } = useParams();
   const count = Number(pId.slice(1)) - 1;
-  if (count) return getSinglePageContents(content, count * 15, count * 15 + 15);
-
+  // The start index.
+  const start = count * 15;
+  // The end index.
+  const end = count * 15 + 15;
+  if (count) return getSinglePageContents(content, start, end);
   return <h1>sorry</h1>;
 };
 export default SplitPage;
