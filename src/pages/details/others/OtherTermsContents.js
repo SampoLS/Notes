aa8c5736b17decc4404704e62dd720715ef3b1ba
SampoLS <mr.sampo@outlook.com>
2022-08-22 19:35:44 +0800

    updated

--- src/pages/details/others/OtherTermsContents.js
+++ src/pages/details/others/OtherTermsContents.js
@@ -2818,4 +2818,29 @@ export const termsContents = [
       ),
     },
   },
+  {
+    id: uuidv4(),
+    title: "Process (engineering)",
+    isLargerTitle: false,
+    path: "/other_terms",
+    url: `${wikiUrl}/Process_(engineering)`,
+    anchor: "Process_(engineering)",
+    text: {
+      p1: (
+        <p>
+          In engineering, a process is a series of interrelated tasks that,
+          together, transform inputs into a given output. These tasks may be
+          carried out by people, nature or machines using various resources; an
+          engineering process must be considered in the context of the agents
+          carrying out the tasks and the resource attributes involved. Systems
+          engineering normative documents and those related to Maturity Models
+          are typically based on processes, for example, systems engineering
+          processes of the EIA-632 and processes involved in the Capability
+          Maturity Model Integration (CMMI) institutionalization and improvement
+          approach. Constraints imposed on the tasks and resources required to
+          implement them are essential for executing the tasks mentioned.
+        </p>
+      ),
+    },
+  },
 ];
