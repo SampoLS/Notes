29f26ab63ca5354ffca1b26e2bbb4a32c7a5e1b6
SampoLS <mr.sampo@outlook.com>
2022-08-31 21:55:19 +0800

    updated

--- src/pages/details/mathematics/mathContents.js
+++ src/pages/details/mathematics/mathContents.js
@@ -223,4 +223,22 @@ export const mathContents = [
       ),
     },
   },
+  {
+    id: uuidv4(),
+    title: "Calculus",
+    isLargerTitle: false,
+    path: "/math",
+    url: `${wikiUrl}/Calculus`,
+    anchor: "Calculus",
+    text: {
+      p1: (
+        <p>
+          Calculus, originally called infinitesimal calculus or "the calculus of
+          infinitesimals", is the mathematical study of continuous change, in
+          the same way that geometry is the study of shape, and algebra is the
+          study of generalizations of arithmetic operations.
+        </p>
+      ),
+    },
+  },
 ];
