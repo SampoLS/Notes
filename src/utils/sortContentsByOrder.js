c49c4ae08d61e842a4445127290394da0996d9f9
SampoLS <mr.sampo@outlook.com>
2023-03-03 00:32:31 +0800

    updated

--- src/utils/sortContentsByOrder.js
+++ src/utils/sortContentsByOrder.js
@@ -5,39 +5,48 @@
  */
 
 export const sortContentsByOrder = (contents) => {
-  const sortedContents = contents.sort((a, b) => {
-    const titleA = a.title.toUpperCase();
-    const titleB = b.title.toUpperCase();
 
-    if (titleA < titleB) return -1;
-    if (titleA > titleB) return 1;
-    return 0;
-  });
+    const sortedContents = contents.sort((a, b) => {
 
-  // Get the index of the content of the large title .
-  const largeTitleIndex = sortedContents.findIndex(
-    (item) => item.isLargerTitle === true
-  );
-  // If the largeTitle exists.
-  if (largeTitleIndex !== -1) {
-    // Delete the content of large(h1) title.
-    const [largeTitle] = sortedContents.splice(largeTitleIndex, 1);
-    // Add the content of large(h1) title to the first position.
-    sortedContents.unshift(largeTitle);
-  }
+        const titleA = a.title.toUpperCase();
+        const titleB = b.title.toUpperCase();
 
-  // Get the index of the content of the Summary title.
-  const summaryTitleIndex = sortedContents.findIndex(
-    (item) => item.title === "Summary"
-  );
-  // If the summaryTitle exsits.
-  if (summaryTitleIndex !== -1) {
-    // Delete the content of Summary title.
-    const [summaryTitle] = sortedContents.splice(summaryTitleIndex, 1);
-    // Add the content of Summary title to the last position.
-    sortedContents.push(summaryTitle);
-  }
+        if (titleA < titleB) return -1;
+        if (titleA > titleB) return 1;
 
-  // Return the sorted contents.
-  return sortedContents;
+        return 0;
+
+    });
+
+    // Get the index of the content of the large title .
+    const largeTitleIndex = sortedContents.findIndex( (item) => item.isLargerTitle === true );
+
+    // If the largeTitle exists.
+    if (largeTitleIndex !== -1) {
+
+        // Delete the content of large(h1) title.
+        const [largeTitle] = sortedContents.splice(largeTitleIndex, 1);
+
+        // Add the content of large(h1) title to the first position.
+        sortedContents.unshift(largeTitle);
+
+    }
+
+    // Get the index of the content of the Summary title.
+    const summaryTitleIndex = sortedContents.findIndex( (item) => item.title === "Summary" );
+
+    // If the summaryTitle exsits.
+    if (summaryTitleIndex !== -1) {
+
+        // Delete the content of Summary title.
+        const [summaryTitle] = sortedContents.splice(summaryTitleIndex, 1);
+
+        // Add the content of Summary title to the last position.
+        sortedContents.push(summaryTitle);
+
+    }
+
+    // Return the sorted contents.
+    return sortedContents;
+
 };
