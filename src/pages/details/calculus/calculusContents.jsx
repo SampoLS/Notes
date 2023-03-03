76d7e22eb77581a6223234fc6467e695d4fc2b91
SampoLS <mr.sampo@outlook.com>
2023-03-03 21:38:47 +0800

    updated

--- src/pages/details/calculus/calculusContents.jsx
+++ src/pages/details/calculus/calculusContents.jsx
@@ -15,6 +15,28 @@ export const calculusContents = [
           p1: <p>Calculus, originally called infinitesimal calculus or "the calculus of infinitesimals", is the mathematical study of continuous change, in the same way that geometry is the study of shape, and algebra is the study of generalizations of arithmetic operations.</p>
         },
     },
+    {
+        id: uuidv4(),
+        title: "Derivative",
+        isLargerTitle: false,
+        path: "/calculus",
+        url: `${wikiUrl}/Derivative`,
+        anchor: "Derivative",
+        text: {
+          p1: <p>In mathematics, the derivative of a function of a real variable measures the sensitivity to change of the function value (output value) with respect to a change in its argument (input value). Derivatives are a fundamental tool of calculus. For example, the derivative of the position of a moving object with respect to time is the object's velocity: this measures how quickly the position of the object changes when time advances.</p>
+        },
+    },
+    {
+        id: uuidv4(),
+        title: "Differential calculus",
+        isLargerTitle: false,
+        path: "/calculus",
+        url: `${wikiUrl}/Differential_calculus`,
+        anchor: "Differential_calculus",
+        text: {
+          p1: <p>In mathematics, differential calculus is a subfield of calculus that studies the rates at which quantities change.[1] It is one of the two traditional divisions of calculus, the other being integral calculus—the study of the area beneath a curve.</p>
+        },
+    },
 ]
 
 export const contentsLength = calculusContents.length;
